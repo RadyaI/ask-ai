40bd3b8853e4b5918f04a9c18839d66f4fa9eff8
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2024-07-11 21:05:01 +0700

    Atur atur dikit

---
+++ pages/halo.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        Ini halaman halo
+        <nuxt-link to="/">balik</nuxt-link>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="landing-page">
+        <h1>Selamat Datang di {{ state.title }}</h1>
+        <p>Ini adalah halaman landing page Anda.</p>
+        <router-link to="/halo">ke halo</router-link>
+    </div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+
+export default {
+    setup() {
+        const state = reactive({
+            title: 'Home'
+        })
+
+        return { state }
+    }
+}
+</script>
+
+<style scoped>
+.landing-page {
+    text-align: center;
+    margin-top: 50px;
+}
+</style>
