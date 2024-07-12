2d06a6247dc51ffb1d15f2c298331d18e460acf3
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2024-07-12 22:02:19 +0700

    styling home

--- pages/halo.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div>
-        Ini halaman halo
-        <nuxt-link to="/">balik</nuxt-link>
-    </div>
-</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,32 @@
 <template>
-    <div class="landing-page">
-        <h1>Selamat Datang di {{ state.title }}</h1>
-        <p>Ini adalah halaman landing page Anda.</p>
-        <router-link to="/halo">ke halo</router-link>
+    <div>
+        <div class="container">
+            <Navbar />
+            <main>
+                <div class="wrapper">
+                    <h2><span style="color: var(--secondary-color);">Ask</span>.Ai</h2>
+                    <div class="ask-input" :style="{'height' : state.beforeAsk ? '70%' : '10%', 'transition' : !state.beforeAsk ? 'height 1s' : 'none'}">
+                        <input type="text" placeholder="Ask...">
+                        <button class="btn-ask"><i class="fa-solid fa-arrow-up"></i></button>
+                    </div>
+                </div>
+            </main>
+        </div>
     </div>
 </template>
 
 <script>
 import { reactive } from 'vue';
+import Navbar from '../layouts/navbar.vue';
 
 export default {
+    components: {
+        Navbar
+    },
     setup() {
         const state = reactive({
-            title: 'Home'
+            title: 'Home',
+            beforeAsk: true
         })
 
         return { state }
@@ -20,9 +34,7 @@ export default {
 }
 </script>
 
-<style scoped>
-.landing-page {
-    text-align: center;
-    margin-top: 50px;
-}
+<style>
+@import url("/assets/css/style.css");
+@import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css");
 </style>
